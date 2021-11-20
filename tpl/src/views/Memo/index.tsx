1b437a30ea28a7a0e8b10b4bda2ed180c035ef87
llqfront <llqfront@aliyun.com>
2021-11-20 21:42:22 +0800

    up

--- tpl/src/views/Memo/index.tsx
+++ tpl/src/views/Memo/index.tsx
@@ -1,15 +1,36 @@
 import React, { memo } from 'react';
 
-interface Props{
+// interface Props{
+//
+// }
+//
+// const View : React.Fc<Props> = (props) =>{
+//     return (
+//         <div>
+//
+// 1111
+//         </div>
+//     )
+// }
+// export default memo(View);
 
+type Props = typeof defaultPorps & PartialProps;
+
+interface PartialProps{
+    count?:number;
 }
-
+const defaultProps = {
+    name:'',
+    age:3,
+}
 const View : React.Fc<Props> = (props) =>{
     return (
         <div>
-
-1111
+            1111
+            {props.age}
         </div>
     )
 }
-export default View;
+View.defaultProps = defaultProps;
+
+export default memo(View);
