0ffa467425c882622d351e798469054adbaf95eb
llqfront <llqfront@aliyun.com>
2021-11-22 11:46:56 +0800

    epic ok 了

--- tpl/src/setupProxy.js
+++ tpl/src/setupProxy.js
@@ -2,6 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(
       '/home',createProxyMiddleware({
+          // 学习调试用
         target: 'https://home-api.pinduoduo.com/',
         changeOrigin: true,
       })
